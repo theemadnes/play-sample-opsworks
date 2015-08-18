e6218192d1cfc6175119c6073941f20981e954b7
theemadnes <alex.mattson@gmail.com>
2015-08-17 18:36:06 -0700

    initial commit

---
+++ app/metadata.rb
@@ -0,0 +1,9 @@
+name             'app'
+maintainer       'theemadnes'
+maintainer_email 'alex.mattson@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures app'
+long_description 'Installs/Configures app'
+version          '0.1.0'
+
+depends "play2", "~> 0.1.0" # add a dependency on Originate's play2 cookbook
